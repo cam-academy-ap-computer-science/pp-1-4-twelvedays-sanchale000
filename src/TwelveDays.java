c17838b3ac8f6842fb02b51b8498d6ac943006e3
sanchale000 <sanchale000@CAM-LAB-24.bgsd.k12.wa.us>
2019-09-19 07:49:42 -0700

    Days

--- src/TwelveDays.java
+++ src/TwelveDays.java
@@ -2,8 +2,54 @@
 public class TwelveDays {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		day12();
 
 	}
-
+	public static void day1() {
+		System.out.println("a partdge in a pear tree.\n");
+	}
+	public static void day2() {
+		System.out.println("two turtle doves, and");
+		day1();
+	}
+	public static void day3() {
+		System.out.println("three french hens,");
+		day2();
+	}
+	public static void day4() {
+		System.out.println("four calling birds,");
+		day3();
+	}
+	public static void day5() {
+		System.out.println("five golden rings,");
+		day4();
+	}
+	public static void day6() {
+		System.out.println("six gees a-laying,");
+		day5();
+	}
+	public static void day7() {
+		System.out.println("seven swans a-swimming,");
+		day6();
+	}
+	public static void day8() {
+		System.out.println("eight maids a-milking,");
+		day7();
+	}
+	public static void day9() {
+		System.out.println("nine ladies dancing,");
+		day8();
+	}
+	public static void day10() {
+		System.out.println("ten lords a-leaping,");
+		day9();
+	}
+	public static void day11() {
+		System.out.println("eleven pipers piping,");
+		day10();
+	}
+	public static void day12() {
+		System.out.println("Twelve drummers drumming,");
+		day11();
+	}
 }
